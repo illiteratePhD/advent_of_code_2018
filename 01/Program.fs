b21ca219e35896a569478e2d7de652fccf9cac0b
Patrik Malm <patrik.Malm@vironova.com>
2018-12-01 09:48:21 +0100

    First part of the problem solved

--- 01/Program.fs
+++ 01/Program.fs
@@ -1,11 +1,29 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-module HelloSquare
 
-let square x = x * x
+
+module SumOfDeltas
+
+open System.IO
+
+let readDeltas filePath = File.ReadLines(filePath)
+
+let tryParseInt s =
+    match System.Int32.TryParse s with
+    | true, n -> Some n
+    | false, _ -> None
+
+let linesToList readValues = 
+    Seq.choose tryParseInt readValues |> Seq.toList
+
+let rec addDeltas = function
+    | [] -> 0
+    | x::xs -> x + addDeltas xs
+
+let deltas = readDeltas("input.txt");
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%d squared is: %d!" 12 (square 12)
+    printfn "Sum of deltas is: %d" (readDeltas "input.txt" |> linesToList |> addDeltas)
     0 // return an integer exit code
