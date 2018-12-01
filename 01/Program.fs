e1870f283e82e98eb0dce82a6bdf4b1bbc742dbc
Patrik Malm <patrik.Malm@vironova.com>
2018-12-01 17:47:04 +0100

    Added a better solution to part two that does not create a new list at each iteration. About four times faster

--- 01/Program.fs
+++ 01/Program.fs
@@ -30,16 +30,32 @@ let rec sumOfDeltas = function
 
 // Second assignment, first recurring frequency
 
-let rec firstDuplicateFrequency (deltas:List<int>, visitedFrequencies:Set<int>, currentFrequency: int) = 
+let rec firstDuplicateFrequency (currentIndex:int, deltas:List<int>, visitedFrequencies:Set<int>, currentFrequency: int) = 
+    match visitedFrequencies.Contains currentFrequency with
+    | true -> currentFrequency
+    | false -> 
+        let nextIndex = match deltas.Length = (currentIndex + 1) with
+                        | true -> 0
+                        | false -> (currentIndex + 1)
+        firstDuplicateFrequency (nextIndex, deltas, (visitedFrequencies.Add currentFrequency), (currentFrequency + deltas.[currentIndex]))
+
+let rec firstDuplicateFrequency' (deltas:List<int>, visitedFrequencies:Set<int>, currentFrequency: int) = 
     match visitedFrequencies.Contains currentFrequency with
     | true -> currentFrequency
     | false -> match deltas with
                | [] -> 0
-               | x::xs -> firstDuplicateFrequency (xs @ [x], (visitedFrequencies.Add currentFrequency), (currentFrequency + x))
+               | x::xs -> firstDuplicateFrequency' (xs @ [x], (visitedFrequencies.Add currentFrequency), (currentFrequency + x))
 
 [<EntryPoint>]
 let main argv = 
     let deltas = getListOfDeltas "input.txt"
     printfn "Sum of deltas is: %d" (sumOfDeltas deltas)
-    printfn "First duplicate frequency is: %d" (firstDuplicateFrequency (deltas, Set.empty, 0))
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    printfn "First duplicate frequency (method 1) is: %d" (firstDuplicateFrequency' (deltas, Set.empty, 0))
+    stopWatch.Stop()
+    printfn "Method 1 time: %f" stopWatch.Elapsed.TotalMilliseconds
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    printfn "First duplicate frequency (method 2) is: %d" (firstDuplicateFrequency (0, deltas, Set.empty, 0))
+    stopWatch.Stop()
+    printfn "Method 2 time: %f" stopWatch.Elapsed.TotalMilliseconds
     0 // return an integer exit code
