ca9fd8a756f5d0bf3aa44ba09ddd0b733b16c0a5
Patrik Malm <patrik.Malm@vironova.com>
2018-12-04 21:23:32 +0100

    Solved part 2 of task 2

--- 02/Program.fs
+++ 02/Program.fs
@@ -27,8 +27,39 @@ let rec countTwosAndThrees (tags, twos, threes) =
  
 let getHash (a, b) = a * b 
 
+let distanceMetric (a, b) = 
+    match a = b with
+    | true -> 0
+    | false -> 1
+
+let distance a b = Seq.sumBy(fun a -> distanceMetric a) <| Seq.zip a b
+
+let rec allToAllDistanceSearch = function
+    | [] -> ("", "")
+    | (x::xs) -> match xs |> List.tryFind(fun s' -> (distance s' x = 1)) with
+                 | Some y -> (x, y)
+                 | None -> allToAllDistanceSearch(xs)
+
+let fst (f, _) = f
+
+let zipped (a, b) = Seq.zip a b
+
+let rec removeNonDuplicate (i:list<char * char>, o:string) = 
+    match i with
+    | [] -> o
+    | (x::xs) -> match distanceMetric x = 0 with 
+                 | true -> removeNonDuplicate(xs, o + string (fst x))
+                 | false -> removeNonDuplicate(xs, o)
+
 [<EntryPoint>]
 let main argv = 
     let twosAndThrees = countTwosAndThrees("input.txt" |> readTags, 0, 0)
     printfn "Hash for list = %d" (getHash twosAndThrees)
+
+    let closestPair = "input.txt" |> readTags |> allToAllDistanceSearch 
+    printfn "Closest pair: %s %s" <|| closestPair
+
+    let correctChars = removeNonDuplicate(zipped closestPair |> Seq.toList, "")
+    printfn "Tag without bad char: %s" correctChars
+
     0 // return an integer exit code
