80187914e0a295e4a6ad4fde48bfd430360cfe18
Patrik Malm <patrik.Malm@vironova.com>
2018-12-07 23:41:46 +0100

    Task 3 finished

--- 03/Program.fs
+++ 03/Program.fs
@@ -2,19 +2,17 @@
 
 open System.IO
 open System.Text.RegularExpressions
-open System.Collections.Specialized
 
-type Point (x:int, y:int) =
-    member this.X = x
-    member this.Y = y
-
-type Rect (x:int, y:int, width:int, height:int) = 
-    
+type Rect (id:int, x:int, y:int, width:int, height:int) = 
+    member this.Id = id
     member this.X = x
     member this.Y = y
     member this.Width = width
     member this.Height = height  
-
+    member this.Top = this.Y
+    member this.Left = this.X
+    member this.Right = this.X + this.Width - 1
+    member this.Bottom = this.Y + this.Height - 1
     member this.IsInside (x, y) = 
         if (x < this.X) then 
             false
@@ -26,6 +24,11 @@ type Rect (x:int, y:int, width:int, height:int) =
             false
         else 
             true
+    
+    member this.Intersects (o:Rect) =
+        (this.Left <= o.Right) && (this.Right >= o.Left) &&
+        (this.Top <= o.Bottom) && (this.Bottom >= o.Top)
+        
 
 let (|CompiledMatch|_|) pattern input =
     if input = null then None
@@ -36,8 +39,8 @@ let (|CompiledMatch|_|) pattern input =
 
 let parseRect s = 
     match s with
-    |   CompiledMatch @"^#[0-9]+ @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)$" [_; x; y; w; h] ->
-        Some(new Rect(x.Value |> int, y.Value |> int, w.Value |> int, h.Value |> int))
+    |   CompiledMatch @"^#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)$" [_; i; x; y; w; h] ->
+        Some(new Rect(i.Value |> int, x.Value |> int, y.Value |> int, w.Value |> int, h.Value |> int))
     | _ -> 
         None
         
@@ -49,7 +52,6 @@ let rec parseRectsFromInputs entries rects =
     | (x::xs) -> 
         let rects = rects @ [(parseRect x).Value]
         parseRectsFromInputs xs rects
-
         
 let isPatchOverlapped (p:int*int, r:list<Rect>) = 
     let r = List.toArray r
@@ -68,6 +70,12 @@ let countOverlappedPatches w h rects =
                                     | true -> 1
                                     | false -> 0 }
     Seq.sum results
+                          
+let rec allToAllOverlapSearch (l:array<Rect>, current: int) =
+    let result = l |> Array.tryFind (fun r -> (r.Id <> l.[current].Id) && r.Intersects(l.[current])) 
+    match result with
+    | Some x -> allToAllOverlapSearch(l, current + 1)
+    | None -> l.[current]                 
 
 [<EntryPoint>]
 let main argv = 
@@ -76,6 +84,13 @@ let main argv =
 
     let entries = "input.txt" |> readInput
     let rectangles = parseRectsFromInputs entries List<Rect>.Empty
-    printfn "Overlap counts = %A" (countOverlappedPatches fabricWidth fabricHeight rectangles)
+
+    //printfn "Overlap counts = %A" (countOverlappedPatches fabricWidth fabricHeight rectangles)
+
+    let nonOverlapping = allToAllOverlapSearch(List.toArray rectangles, 0)
+
+    printfn "Non-overlapping rect = %A" nonOverlapping.Id
+
+
 
     0 // return an integer exit code
